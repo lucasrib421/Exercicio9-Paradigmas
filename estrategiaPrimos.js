7b8b64e9d0eb941b2a02f50b8fb2583fbc1b4c0d
GabrielDavi7 <133395251+GabrielDavi7@users.noreply.github.com>
2025-06-13 21:55:58 -0300

    Update estrategiaPrimos.js

--- estrategiaPrimos.js
+++ estrategiaPrimos.js
@@ -1,1 +1,14 @@
+function ehPrimo(numero) {
+  if (numero < 2) return false;
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) return false;
+  }
+  return true;
+}
+
+function filtrarPrimos(lista) {
+  return lista.filter(ehPrimo);
+}
+
+module.exports = filtrarPrimos;
 
